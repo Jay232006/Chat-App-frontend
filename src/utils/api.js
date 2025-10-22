2bbdcba246d0988e462137561432659826da7836
unknown <jayeshnc11@gmail.com>
2025-10-22 15:58:11 +0530

    socket.io changes

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://chat-app-backend-31vq.onrender.com",
 });
 
 export default API;
